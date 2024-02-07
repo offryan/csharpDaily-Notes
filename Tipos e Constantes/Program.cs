4b5efe96b4d4a4ce57af18afd1a38c027f8ada95
DEV RYAN <85769101+offryan@users.noreply.github.com>
2024-02-07 13:23:26 -0300

    Tipos de dados e Constantes

---
+++ Tipos e Constantes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Dar preferência para letras minúsculas, para usar menos memória
+
+            float real1 = 100.75f; // 32 bits, usar o "F" ou "f"
+            double real2 = -125.99; // Aceita números negativos e positvos 64 bits
+            decimal real3 = 752548.453m; // usar o "M" ou "m" 128 bits 
+
+            Console.WriteLine("Valores " +
+                "Float: " + real1 + 
+                "|  Double: " + real2 +
+                "|  Decimal: " + real3);
+
+            // Constantes são valores imútaveis
+
+            const double pi = 3.1415;   
+            const string name = "Ryan";
+
+            Console.WriteLine(pi + name);
+            Console.ReadKey();
+        }
+    }
+}
