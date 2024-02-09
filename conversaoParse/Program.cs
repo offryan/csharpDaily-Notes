431a1b459bfe2d4d74ad8627a87349014d84767d
DEV RYAN <85769101+offryan@users.noreply.github.com>
2024-02-09 13:17:43 -0300

    Conversão com Parse

--- conversaoParse/Program.cs
+++ conversaoParse/Program.cs
@@ -17,7 +17,7 @@ namespace conversaoParse
 
             double num2 = double.Parse("1249,53"); // Vírgula em vez do ponto, se não printará como o número 124953.
 
-            float num3 = float.Parse("1717,17"); // Float não precisa da letra ´f
+            float num3 = float.Parse("1717,17"); // Float não precisa da letra f
 
             Console.WriteLine(numero);
             Console.WriteLine(num1);
