bbf493f0ea417a284364c8db76f33d9be16dfa94
DEV RYAN <85769101+offryan@users.noreply.github.com>
2024-02-09 13:18:43 -0300

    Conversão com Convert
    Conversão com Convert

---
+++ conversaoConvert/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace convert
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string txt = Convert.ToString(2500);
+
+            double num1 = Convert.ToDouble(false);
+
+            int num2 = Convert.ToInt32('C'); // utiliza as aspas simples
+
+            Console.WriteLine(txt);
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+            Console.ReadKey();
+        }
+    }
+}
