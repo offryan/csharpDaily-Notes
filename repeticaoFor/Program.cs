9cec430026a0af022a10b74e3217167004e8ab9e
DEV RYAN <85769101+offryan@users.noreply.github.com>
2024-02-16 21:47:31 -0300

    Repetição - For
    Repetição - For

---
+++ repeticaoFor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace repeticaoFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 0, j = 10; i < 10; i++, j--)
+            //{
+            //    Console.WriteLine("I = : " + i + " | J = " + j);
+            //}
+
+            string[] nomes = { "Maça","Pera","Banana","Uva" };
+
+            foreach (string nome in nomes)
+            {
+                Console.WriteLine("Nome: " + nome);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
