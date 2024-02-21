a448f2f09e15792cb0e8719c3ddfb476d45bc87d
DEV RYAN <85769101+offryan@users.noreply.github.com>
2024-02-21 13:17:42 -0300

    Métodos Simples

---
+++ metodos/Poo_classesObjetos/Pessoa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poo_classesObjetos
+{
+    internal class Pessoa
+    { 
+        // Atributos / Campos
+        public string nome;
+        public string sobrenome;
+        public int anoNascimento;
+
+        // Método simples
+        public void cumprimentar()
+        {
+            Console.WriteLine("Olá, eu sou " + nome + " " + sobrenome);
+        }
+    }
+}
---
+++ metodos/Poo_classesObjetos/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poo_classesObjetos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Pessoa p1 = new Pessoa()
+            { nome = "Ryan", sobrenome = "Gomes", anoNascimento = 2002 };
+
+            Pessoa p2 = new Pessoa()
+            { nome = "Murilo", sobrenome = "Gomes", anoNascimento = 2007 };
+            p2.sobrenome = "Paulino"; // Alteração de valor
+
+            Pessoa p3 = new Pessoa()
+            { nome = "Vitor", sobrenome = "Melo", anoNascimento = 2004 };
+            p3.anoNascimento = 2008; // Método Cumprimentar
+
+            Console.WriteLine();
+
+            Console.WriteLine("Nome: " + p1.nome + " " + p1.sobrenome + " | Ano de Nascimento: " + p1.anoNascimento);
+            p1.cumprimentar(); // Método Cumprimentar
+
+            Console.WriteLine();
+
+            Console.WriteLine("Nome: " + p2.nome + " " + p2.sobrenome + " | Ano de Nascimento: " + p2.anoNascimento);
+            p2.cumprimentar(); // Método Cumprimentar
+
+            Console.WriteLine();
+
+            Console.WriteLine("Nome: " + p3.nome + " " + p3.sobrenome + " | Ano de Nascimento: " + p3.anoNascimento);
+            p3.cumprimentar(); /// Método Cumprimentar
+
+            Console.ReadKey();
+        }
+    }
+}
