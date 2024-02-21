9801706d6f13de0f84f7e4e7ddc1d74530860244
DEV RYAN <85769101+offryan@users.noreply.github.com>
2024-02-21 13:23:06 -0300

    Parâmentros por Valor e Referência

---
+++ metodos/metodosDasClasses/Metodos.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metodosDasClasses
+{
+    internal class Metodos
+    {
+        // Métodos com Simples
+        public void Cumprimentar()
+        {
+            Console.WriteLine("Olá, bom dia. ");
+        }
+
+        // Métodos com Parâmentros
+
+        // Somar
+        public void Somar(int num1, int num2)
+        {
+            int result = num1 + num2;
+            Console.WriteLine("A soma é: " + result);
+        }
+        // Apresentar
+        public void Apresentar(string nome, int idade)
+        {
+            Console.WriteLine("Meu nome é: " + nome + " e tenho " + idade + " anos");
+        }
+
+        // Parâmentros por valor
+
+        public void AumentarValor(int valor)
+        {
+            valor += 10;
+            Console.WriteLine("Valor final (por valor) é: " + valor);
+        }
+
+        // Parâmentros por referência
+        public void AumentarRef(ref int valor)
+        {
+            valor += 10;
+            Console.WriteLine("Valor final (por referência) é: " + valor);
+        }
+
+        // Métodos com retorno de valores
+        public string MontaNome(string nome, string sobrenome)
+        {
+            // string nomeCompleto = nome + " " + sobrenome;
+            // return nomeCompleto;
+
+            return nome + " " + sobrenome; // Simplificado
+        }
+
+        public int CodigoChar(char caractere)
+        {
+            // int codigo = (int)caractere; -----> conversão redundante implicita
+            return caractere;
+        }
+
+        public double PI()
+        {
+            return 3.1415;
+        }
+
+    }
+}
---
+++ metodos/metodosDasClasses/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metodosDasClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Metodos m = new Metodos();
+
+            /*          Métodos Simples
+
+                        m.Cumprimentar();
+                        m.Somar(10, 40);
+                        m.Apresentar("Ryan",21);
+            */
+
+            /* Parâmentros por valor e referência
+            int valor1 = 100;
+            int valor2 = 100;
+
+            m.AumentarValor(valor1); // Por valor
+            m.AumentarRef(ref valor2); // Por referência
+            Console.WriteLine("Valor 1: " + valor1);
+            Console.WriteLine("Valor 2: " + valor2);
+                        */
+
+            /* Métodos com retorno de valores
+            string nomeCompleto =  m.MontaNome("Ryan", "Gomes");
+            int codigoChar = m.CodigoChar('r');
+            double pi = m.PI();
+
+            Console.WriteLine("Nome completo: " + nomeCompleto);
+            Console.WriteLine("Código Char: " + codigoChar);
+            Console.WriteLine("Valor de PI: " + pi);
+            */
+
+            Console.ReadKey();
+        }
+    }
+}
